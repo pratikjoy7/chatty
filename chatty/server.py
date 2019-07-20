6ddb704a8ddda0a689559bc8e89ed83c246de3f7
Pratik Saha <pratikjoy7@gmail.com>
2019-07-20 23:58:46 +0600

    Add server.py for starting the project server

---
+++ chatty/server.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, render_template, session, redirect, url_for
+from flask_socketio import SocketIO, emit
+
+from chatty.config import Config
+from chatty.services import authentication
+from chatty.services.authorization import requires_auth
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+app = Flask(__name__)
+app.config.from_object(Config)
+socketio = SocketIO( app )
+
+@app.route('/auth', defaults={'action': 'login'})
+@app.route('/auth/<action>')
+def auth(action):
+    return authentication.authenticate(action)
+
+
+@app.route('/login')
+def login():
+    if session.get('user'):
+        return redirect(url_for('.auth', action='logout', _external=True))
+    return render_template('login.html')
+
+
+@app.route( '/' )
+@requires_auth
+def index():
+  return render_template( 'index.html' )
+
+
+if __name__ == '__main__':
+  socketio.run( app, debug = True )
